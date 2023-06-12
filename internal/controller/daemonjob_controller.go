06ad935005136ee77113a005603cf67ee0b8380c
Mugdha Adhav <mugdha.adhav@gmail.com>
2023-06-12 13:50:38 +0530

    Run pod on all nodes

--- internal/controller/daemonjob_controller.go
+++ internal/controller/daemonjob_controller.go
@@ -10,6 +10,7 @@ import (
 
 	demov1 "github.com/philomathesinc/daemonset-cronjob-hybrid/api/v1"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // DaemonjobReconciler reconciles a Daemonjob object
@@ -22,10 +23,19 @@ type DaemonjobReconciler struct {
 //+kubebuilder:rbac:groups=demo.example.com,resources=daemonjobs/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=demo.example.com,resources=daemonjobs/finalizers,verbs=update
 //+kubebuilder:rbac:groups=core,resources=pods,verbs=create;list;get;watch
-//+kubebuilder:rbac:groups=core,resources=node,verbs=list
+//+kubebuilder:rbac:groups=core,resources=nodes,verbs=list;watch
 
 func (r *DaemonjobReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := log.FromContext(ctx)
+
+	// A variable to hold the CR data
+	var daemonjob demov1.Daemonjob
+
+	// Call kubernetes API to get the CR data
+	if err := r.Get(ctx, req.NamespacedName, &daemonjob); err != nil {
+		log.Error(err, "unable to fetch daemonjob")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
 
 	// Call kubernetes API to get nodes
 	var nodes corev1.NodeList
@@ -36,6 +46,35 @@ func (r *DaemonjobReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 
 	for _, n := range nodes.Items {
 		log.Info(n.Name)
+		pod := &corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      req.Name + "-" + n.Name,
+				Namespace: req.Namespace,
+			},
+			Spec: corev1.PodSpec{
+				NodeName: n.Name,
+				Containers: []corev1.Container{
+					{
+						Name:    "default",
+						Image:   "busybox",
+						Command: []string{"sh", "-c"},
+						Args: []string{
+							"sleep 3600",
+						},
+					},
+				},
+			},
+		}
+
+		if err := ctrl.SetControllerReference(&daemonjob, pod, r.Scheme); err != nil {
+			log.Error(err, "unable to set controller reference")
+		}
+
+		// call k8s api to create the pod
+		if err := r.Create(ctx, pod); err != nil {
+			log.Error(err, "unable to create pod")
+		}
+		log.Info("Pod created", "name", req.Name+"-"+n.Name, "namespace", req.Namespace)
 	}
 
 	// Daemonset -> Pod
